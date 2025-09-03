e0198f513eb5422678b1eae80bc73414ecd6672a
linuxcompas <kalikeylogger00@gmail.com>
2025-09-03 11:33:39 +0200

    tivoli

--- src/TivoliRideTest.java
+++ src/TivoliRideTest.java
@@ -14,6 +14,7 @@ public class TivoliRideTest {
 
                 System.out.print("How tall are you");
                 double height = input.nextDouble();
+                return sc.nextDoubl
 
 
             }
